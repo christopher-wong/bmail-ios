fa307748b35673fa4925ecb3ab621587368bf1ff
Christopher Wong <christopher-wong@users.noreply.github.com>
2026-05-23 00:06:19 -0400

    Initial Commit

---
+++ bmail/bmailApp.swift
@@ -0,0 +1,17 @@
+//
+//  bmailApp.swift
+//  bmail
+//
+//  Created by Christopher Wong on 5/23/26.
+//
+
+import SwiftUI
+
+@main
+struct bmailApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ bmailTests/bmailTests.swift
@@ -0,0 +1,19 @@
+//
+//  bmailTests.swift
+//  bmailTests
+//
+//  Created by Christopher Wong on 5/23/26.
+//
+
+import Testing
+@testable import bmail
+
+struct bmailTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
